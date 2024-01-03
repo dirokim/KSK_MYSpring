effccea6ed3a7340c8871f76fb873389063f4087
dirokim <dirokim7@gmail.com>
2024-01-03 15:18:39 +0900

    ad

--- src/main/java/com/winter/app/product/ProductDAO.java
+++ src/main/java/com/winter/app/product/ProductDAO.java
@@ -2,6 +2,7 @@ package com.winter.app.product;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
 import com.summer.app.util.DBConnector;
@@ -25,15 +26,23 @@ public class ProductDAO {
 		return null;
 		
 	}
-	public void list(){
+	public void list() throws Exception{
 		Connection con = DBConnector.getConnector();
 		String sql = "SELET * FROM PRODUCT";
 		PreparedStatement ps = con.prepareStatement(sql);
 		
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()){
+			ProductDTO productDTO = new ProductDTO();
+			productDTO.setProductNum(rs.getLong("PRODUCTNUM"));
+			productDTO.setProductName(rs.getString("PRODUCTNAME"));
+			productDTO.setProductRate(rs.getDouble(""));
+		}
 		
 		
 		
-		List<ProductDTO> ar = List<ProductDTO>;
+		
 		
 	}
 	
