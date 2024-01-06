efc23aadbba34dd3f1cc99d07401d891117087e4
dirokim <dirokim7@gmail.com>
2024-01-06 22:36:37 +0900

    ad

--- src/test/java/com/winter/app/MyTest.java
+++ src/test/java/com/winter/app/MyTest.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
 public abstract class MyTest {
 
 	
